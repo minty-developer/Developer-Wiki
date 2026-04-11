adfcfa5f0351133488d23cb01fadfcff0cd775ec
minty-developer <gimminseung036@gmail.com>
2026-04-11 12:52:55 +0900

    ReTest Script.js

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ const firebaseConfig = {
   appId: "1:305676986631:web:893587ce1f950fb5a8bda2"
 };
 
-const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 const params = new URLSearchParams(window.location.search);
